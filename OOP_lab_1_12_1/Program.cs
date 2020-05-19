848c5fd38d420e02313e932c93d38dff3b897537
Роман Данилевич <dwordchanel@gmail.com>
2020-05-19 18:18:11 +0300

    Add project files.

---
+++ OOP_lab_1_12_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OOP_lab_1_12_1
+{
+    public class Program
+    {
+        public static double function(int x)
+        {
+            return x * x * x - 4 * x * x - 5 * x + 9 + Math.Cos(x);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("x = ");
+            Console.WriteLine("y = {0}", function(int.Parse(Console.ReadLine())));
+        }
+    }
+}
---
+++ Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(10, OOP_lab_1_12_1.Program.function(0));
+        }
+    }
+}
